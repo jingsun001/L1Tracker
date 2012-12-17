5accdf43aea6c61ada7f99d6ea4b4c91c79f9f6b
sunjing <jingsun001@gmail.com>
2012-12-17 10:50:46 +0800

    修改编译指令，便于项目大家都能调试通过，添加新的任务 需要环境变量 OPENCV 其中包括include和lib目录

--- L1Tracker/stdafx.h
+++ L1Tracker/stdafx.h
@@ -25,17 +25,17 @@ using namespace cv;
 #ifdef _DEBUG
 //Debug Mode
 
-#pragma comment(lib,"E:\\OpenCV\\build\\x86\\vc10\\lib\\opencv_core243d.lib")
-#pragma comment(lib,"E:\\OpenCV\\build\\x86\\vc10\\lib\\opencv_imgproc243d.lib")
-#pragma comment(lib,"E:\\OpenCV\\build\\x86\\vc10\\lib\\opencv_highgui243d.lib")
-#pragma comment(lib,"E:\\OpenCV\\build\\x86\\vc10\\lib\\opencv_imgproc243d.lib")
-#pragma comment(lib,"E:\\OpenCV\\build\\x86\\vc10\\lib\\opencv_objdetect243d.lib")
+#pragma comment(lib,"opencv_core243d.lib")
+#pragma comment(lib,"opencv_imgproc243d.lib")
+#pragma comment(lib,"opencv_highgui243d.lib")
+#pragma comment(lib,"opencv_imgproc243d.lib")
+#pragma comment(lib,"opencv_objdetect243d.lib")
 #else
 //Release Mode
 
-#pragma comment(lib,"E:\\OpenCV\\build\\x86\\vc10\\lib\\opencv_core243.lib")
-#pragma comment(lib,"E:\\OpenCV\\build\\x86\\vc10\\lib\\opencv_imgproc243.lib")
-#pragma comment(lib,"E:\\OpenCV\\build\\x86\\vc10\\lib\\opencv_highgui243.lib")
-#pragma comment(lib,"E:\\OpenCV\\build\\x86\\vc10\\lib\\opencv_imgproc243.lib")
-#pragma comment(lib,"E:\\OpenCV\\build\\x86\\vc10\\lib\\opencv_objdetect243.lib")
+#pragma comment(lib,"opencv_core243.lib")
+#pragma comment(lib,"opencv_imgproc243.lib")
+#pragma comment(lib,"opencv_highgui243.lib")
+#pragma comment(lib,"opencv_imgproc243.lib")
+#pragma comment(lib,"opencv_objdetect243.lib")
 #endif
